0c830051357004c4d1f187c267122f9981f4a5da
Bryan Oyloe <boyloe@gmail.com>
2020-06-18 22:30:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 12
-second_number = 
+second_number = 15
 product = first_number * second_number
 sum = first_number + second_number
 difference = first_number - second_number
